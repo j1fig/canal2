9d9546e03fbe0c43ea4fe35902e2e60f844ce449
Joao Figueiredo <joaonvfigueiredo@gmail.com>
2019-07-13 14:07:54 +0100

    updated deploy to fail on command errors.

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-push_to_remote ()
+
+progress ()
 {
     echo -n "   $1"
-    git push $1 master > /dev/null 2>&1 &
     spin='-\|/'
     PID=$!
     while kill -0 $PID 2> /dev/null; do
@@ -10,13 +10,21 @@ push_to_remote ()
         printf "\r ${spin:$i:1}"
         sleep .1
     done
+    if [ "$2" != "0" ]; then
+        wait $!
+        if [ $? -ne 0 ]; then
+            printf "\r ! "
+            echo
+            echo $2
+            exit 1
+        fi
+    fi
     printf "\r ✔ "
     echo
 }
 
-push_to_remote origin
-push_to_remote heroku
+git push origin master > /dev/null 2>&1 &
+progress "pushing to github" "Probable network problem - try pushing manually."
 
-# if the above doesn't work out well just
-# git push origin master
-# git push heroku master
+git push heroku master > /dev/null 2>&1 &
+progress "deploy to heroku" "Try to deploy manually with git push heroku master."
